969ed7228f84b86f8be2b754d86036c65c179c37
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2024-03-28 12:57:16 +0100

    Variabler, vägg och game over mekanik

--- game-loop.py
+++ game-loop.py
@@ -1,63 +1,131 @@
+# 1 - Import library
 import pygame
 from pygame.locals import *
+from random import randint
 
+# 2 - Initiera spelet
 pygame.init()
 pygame.font.init()
-
-#Bestäm storleken på spelplanen
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 width, height = 64*10, 64*8
+screen=pygame.display.set_mode((width, height))
 
-screen = pygame.display.set_mode((width, height))
-
-player_x = 200
-player_y = 200
+# Player variabler
+player_x = 256
+player_y = 440
 keys = [False, False, False, False]
 
+# Wall variabler
+wall_y = -64
+rand_wall = randint(0,8)
+nmbr_of_walls = 0
+wall_speed = 2
+
+# Kolla om spelet är över eller inte
+is_game = False
+
+# 3 - Ladda bilder
 player = pygame.image.load("zombie.png")
+wall = pygame.image.load("wall.png")
 
+def wall_render(rand, wall_y):
+    for i in range(0, 10):
+        if not (i == rand or i == rand + 1):
+            screen.blit(wall, (i*64, wall_y))
+
+def wall_handler(wall_y, rand, nmbr_of_walls, wall_speed):
+    wall_y += wall_speed
+    if wall_y > height:
+        wall_y = -64
+        rand = randint(0,8)
+        nmbr_of_walls += 1
+        wall_speed +=1
+
+    return wall_y, rand, nmbr_of_walls, wall_speed  
+
+def collide(player_x, player_y, wall_y, rand_wall):
+    if (player_y >= wall_y and player_y <= wall_y + 64) and\
+        ((player_x > 0 and player_x <= rand_wall*64) or (player_x > (rand_wall+1)*64 and player_x < width+64)):
+        return True
+    else:
+        return False
+
+# 4 - Loopa igenom spelet
 while 1:
-    # Rita ut en vit spelplan
-    screen.fill((255, 255, 255))
+    if not is_game:
+        screen.fill((255,255,255))
+        textsurface = myfont.render("Press enter to start", False, (0, 0, 0))
+        screen.blit(textsurface,(180, 200))
+        pygame.display.flip()
 
-    # Rita ut bilden player på en x, y koordinat
-    screen.blit(player, (player_x, player_y))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
 
-    # Updatera spelplanen
-    pygame.display.flip()
+            if event.type == pygame.KEYDOWN:
+                if event.key==K_RETURN:
+                    player_x = 256
+                    player_y = 440
+                    wall_y = -64
+                    rand_wall = randint(0,8)
+                    nmbr_of_walls = 0
+                    keys = [False, False, False, False]
+                    wall_speed = 2
+                    is_game = True
+    else:
+        # 5 - Rensa skärmen innan vi ritar ut igen
+        screen.fill((255,255,255))
+        # 6 - Rita alla element på skärmen (Spelare, vägg)
+        screen.blit(player, (player_x, player_y))
 
-    for event in pygame.event.get(): # Kolla om spelplanen stängs ner. I så fall, stäng av programmet
-        if event.type == pygame.QUIT: # Om det är fallet avsluta programmet helt
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
-            if event.key == K_UP:
-                keys[0] = True
-            elif event.key == K_LEFT:
-                keys[1] = True
-            elif event.key == K_DOWN:
-                keys[2] = True
-            elif event.key == K_RIGHT:
-                keys[3] = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                keys[0] = False
-            elif event.key == pygame.K_LEFT:
-                keys[1] = False
-            elif event.key == pygame.K_DOWN:
-                keys[2] = False
-            elif event.key == pygame.K_RIGHT:
-                keys[3] = False
-            # Updatera y-positionen
-        # Updatera y-positionen
-        if keys[0]:  # Om uppåtknappen är intryckt
-            if player_y > 0:  # Om koordinaten är större än 0 (ej utanför spelplanen)
-                player_y -= 15  # Ändra y-positionen med 15 pixlar. Spelaren åker uppåt
-        elif keys[2]:  # Om nedåttangenten är intryckt
-            if player_y < height-64:  # Om koodinaten är mindre än höjden på spelplanen (ej utanför spelplanen)
-                player_y += 15  # Ändra y-positionen med 15 pixlar. Spelaren åker nedåt
-        if keys[1]:  # Om vänster tangent är nedtryckt
-            if player_x > 0:  # Om spelaren är innanför spelplanen
-                player_x -= 15  # Minska positionen i x-led. Spelaren åker vänster
-        elif keys[3]:  # Om höger tangent är nedtryckt
-            if player_x < width-64:  # Om spelaren är innanför spelplanen
-                player_x += 15  # Öka positionen i x-led. Spelaren åker höger
+        textsurface = myfont.render(str(nmbr_of_walls), False, (0, 0, 0))
+        screen.blit(textsurface,(10, 0))
+
+        wall_y, rand_wall, nmbr_of_walls, wall_speed = wall_handler(wall_y, rand_wall, nmbr_of_walls, wall_speed)
+        wall_render(rand_wall, wall_y)
+
+        if collide(player_x, player_y, wall_y, rand_wall):
+            is_game = False
+
+        # 7 - Updatera skärmen
+        pygame.display.flip()
+        # 8 - Loopa igenom alla event
+        for event in pygame.event.get():
+            # kolla om eventet är X button
+            if event.type == pygame.QUIT:
+                # Om det är, quit the game
+                pygame.quit() 
+                exit(0) 
+            if event.type == pygame.KEYDOWN:
+                if event.key==K_UP:
+                    keys[0]=True
+                elif event.key==K_LEFT:
+                    keys[1]=True
+                elif event.key==K_DOWN:
+                    keys[2]=True
+                elif event.key==K_RIGHT:
+                    keys[3]=True
+
+            if event.type == pygame.KEYUP:
+                if event.key==pygame.K_UP:
+                    keys[0]=False
+                elif event.key==pygame.K_LEFT:
+                    keys[1]=False
+                elif event.key==pygame.K_DOWN:
+                    keys[2]=False
+                elif event.key==pygame.K_RIGHT:
+                    keys[3]=False
+
+        if keys[0]:
+            if player_y > 0:
+                player_y -= 15
+        elif keys[2]:
+            if player_y < height-64:
+                player_y += 15 
+        if keys[1]: 
+            if player_x > 0:
+                player_x -= 15
+        elif keys[3]:
+            if player_x < width-64:
+                player_x += 15

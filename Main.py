2de0b7a791ffce643b1b1d96b37de13ca4868db4
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2024-04-15 14:19:58 +0200

    Formatting code

--- Main.py
+++ Main.py
@@ -7,11 +7,12 @@ pygame.init()
 SW, SH = 600, 600
 
 BLOCK_SIZE = 50
-pygame.font.SysFont('arial', BLOCK_SIZE*2)
+pygame.font.SysFont('arial', BLOCK_SIZE * 2)
 
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Snake!")
 clock = pygame.time.Clock()
+
 
 class Snake:
     def __init__(self):
@@ -24,26 +25,29 @@ class Snake:
 
     def update(self):
         self.body.append(self.head)
-        for i in range(len(self.body)-1):
-            self.body[i].x, self.body[i].y = self.body[i+1].x, self.body[i+1].y
+        for i in range(len(self.body) - 1):
+            self.body[i].x, self.body[i].y = self.body[i + 1].x, self.body[i + 1].y
         self.head.x += self.xdir * BLOCK_SIZE
         self.head.y += self.ydir * BLOCK_SIZE
         self.body.remove(self.head)
 
+
 class Apple:
     def __init__(self):
-        self.x = int(random.randint(0, SW)/BLOCK_SIZE) * BLOCK_SIZE
-        self.y = int(random.randint(0, SH)/BLOCK_SIZE) * BLOCK_SIZE
+        self.x = int(random.randint(0, SW) / BLOCK_SIZE) * BLOCK_SIZE
+        self.y = int(random.randint(0, SH) / BLOCK_SIZE) * BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
 
     def update(self):
         pygame.draw.rect(screen, "red", self.rect)
+
 
 def drawGrid():
     for x in range(0, SW, BLOCK_SIZE):
         for y in range(0, SH, BLOCK_SIZE):
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, "#3c3c3b", rect, 1)
+
 
 drawGrid()
 

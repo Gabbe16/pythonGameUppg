6c6054bc9740e4fbe509f133531512be8fe75e8b
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2024-05-27 14:18:20 +0200

    added Weather api calls

--- Main.py
+++ Main.py
@@ -1,22 +1,23 @@
-# Import statements
 import pygame
 import sys
 import random
+from weather import get_temp
 
 pygame.init()
 
-# Variables for setup
 SW, SH = 600, 600
+
 BLOCK_SIZE = 50
 pygame.font.SysFont('arial', BLOCK_SIZE * 2)
+
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Snake game")
 clock = pygame.time.Clock()
 
-# Snake class with variables and methods
+color = get_temp()
+
 class Snake:
     def __init__(self):
-        # Variables for the snakes body, head, if its dead and position
         self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
         self.xdir = 1
         self.ydir = 0
@@ -25,18 +26,15 @@ class Snake:
         self.dead = False
 
     def update(self):
-        # Global variables
         global apple
         global boostApple
 
-        # Check if the snake collided with itself, if it did it is dead
         for square in self.body:
             if self.head.x == square.x and self.head.y == square.y:
                 self.dead = True
             if self.head.x not in range(0, SW) or self.head.y not in range(0, SH):
                 self.dead = True
 
-        # If the snake died restart the game with a new snake and new apples
         if self.dead:
             self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
             self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
@@ -46,7 +44,6 @@ class Snake:
             self.dead = False
             apple = Apple()
 
-        # Place the snakes head into the body array and check if you have gotten any apples (body length) then remove and reapply the head
         self.body.append(self.head)
         for i in range(len(self.body) - 1):
             self.body[i].x, self.body[i].y = self.body[i + 1].x, self.body[i + 1].y
@@ -54,36 +51,37 @@ class Snake:
         self.head.y += self.ydir * BLOCK_SIZE
         self.body.remove(self.head)
 
-# Apple class with random variables for the spawnpoint
+
 class Apple:
     def __init__(self):
         self.x = int(random.randint(0, SW) / BLOCK_SIZE) * BLOCK_SIZE
         self.y = int(random.randint(0, SH) / BLOCK_SIZE) * BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
-    
-    # Drawing the apple on the screen
+
     def update(self):
         pygame.draw.rect(screen, "red", self.rect)
 
-# BoostedApple has inherited the same variables as Apple but i've overridden the update method to make it blue
+
 class BoostedApple(Apple):
     def update(self):
         pygame.draw.rect(screen, "blue", self.rect)
 
-# Draw the grid for the game on the screen
+
 def drawgrid():
     for x in range(0, SW, BLOCK_SIZE):
         for y in range(0, SH, BLOCK_SIZE):
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
-# Creating game mechanics by calling the classes and creating instances
+
 drawgrid()
+
 snake = Snake()
+
 apple = Apple()
+
 boostApple = BoostedApple()
 
-# Controlling the snake with controls using x and y positions
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -103,27 +101,24 @@ while True:
                 snake.ydir = 0
                 snake.xdir = -1
 
-    screen.fill('black')
+    snake.update()
+
+    screen.fill(color)
     drawgrid()
 
-    # Calling update methods for the snake, apple and boostedApple
-    snake.update()
     apple.update()
+
     boostApple.update()
 
-    # Draw the head of the snake on the screen
     pygame.draw.rect(screen, "green", snake.head)
 
-    # Draw the body of the snake on the screen
     for square in snake.body:
         pygame.draw.rect(screen, "green", square)
 
-    # Snake body extension by colliding with regular apple
     if snake.head.x == apple.x and snake.head.y == apple.y:
         snake.body.append(pygame.Rect(square.x, square.y, BLOCK_SIZE, BLOCK_SIZE))
         apple = Apple()
 
-    # Snake body extension by colliding with BoostedApple
     if snake.head.x == boostApple.x and snake.head.y == boostApple.y:
         snake.body.append(pygame.Rect(square.x, square.y, BLOCK_SIZE, BLOCK_SIZE))
         boostApple = BoostedApple()
---
+++ weather.py
@@ -0,0 +1,39 @@
+import requests
+
+def get_weather(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] == 200:
+        temperature = data["main"]["temp"]
+        return temperature
+    else:
+        print("City not found.")
+
+def main():
+    city = input("Enter city name: ")
+    api_key = "f3766672014224dca721272a510aec96"  # Replace this with your actual API key
+    get_weather(city, api_key)
+
+if __name__ == "__main__":
+    main()
+
+def get_temp():
+    city = input("Enter city name: ")
+    api_key = "f3766672014224dca721272a510aec96"
+    temp = get_weather(city, api_key)
+    print(temp)
+    temp = round(temp)
+    if temp >= 30:
+        color = (139, 64, 0)
+        return color
+    elif temp >= 20:
+        color = (204, 85, 0)
+        return color
+    elif temp >= 0:
+        color = (255, 255, 255)
+        return color
+    elif temp < 0:
+        color = (0, 0, 255)
+        return color
